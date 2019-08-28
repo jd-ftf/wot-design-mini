5def8b272fb8a0ca1c2a18a1bfe90bf0f553313a
zhaixinhua <zhaixinhua@jd.com>
2019-08-28 21:39:24 +0800

    button 组件scss优化，loading与icon未添加

--- packages/button/index.js
+++ packages/button/index.js
@@ -1,4 +1,5 @@
 Component({
+  externalClasses: ["hover-class"],
   properties: {
     plain: Boolean,
     disabled: Boolean,
@@ -9,8 +10,60 @@ Component({
     },
     size: {
       type: String,
-      value: "large"
+      value: "middle"
     },
-    openType: String
+    openType: String,
+    formType: String,
+    hoverStopPropagation: {
+      type: Boolean,
+      value: false
+    },
+    lang: {
+      type: String,
+      value: "en"
+    },
+    sessionFrom: String,
+    sendMessageTitle: String,
+    sendMessagePath: String,
+    sendMessageImg: String,
+    appParameter: String,
+    showMessageCard: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+    hoverStartTime: 20,
+    hoverStayTime: 70
+  },
+  methods: {
+    onClick() {
+      if (!this.data.disabled && !this.data.loading) {
+        this.triggerEvent("click");
+      }
+    },
+    bindGetUserInfo(event) {
+      this.triggerEvent("getuserinfo", event.detail);
+    },
+
+    bindContact(event) {
+      this.triggerEvent("contact", event.detail);
+    },
+
+    bindGetPhoneNumber(event) {
+      this.triggerEvent("getphonenumber", event.detail);
+    },
+
+    bindError(event) {
+      this.triggerEvent("error", event.detail);
+    },
+
+    bindLaunchApp(event) {
+      this.triggerEvent("launchapp", event.detail);
+    },
+
+    bindOpenSetting(event) {
+      this.triggerEvent("opensetting", event.detail);
+    }
   }
 });
