9db5e39ce5505dafa6917dc70774e8c1448a6acc
houxinchao <3104288823@qq.com>
2019-09-06 14:36:57 +0800

    删除多余样式

--- example/pages/inputNumber/inputNumber.js
+++ example/pages/inputNumber/inputNumber.js
@@ -4,9 +4,6 @@ Page({
     value: 1,
     value1:1,
   },
-  handleBlur(){
-    console.log("这是在父组件里的blur");
-  },
   handleChange1 ({ detail }) {
     this.setData({
       value1: detail.value
@@ -24,7 +21,6 @@ Page({
 
     setTimeout(function () {
       wx.hideToast();
-      console.log("延迟调用============");
       _this.setData({
         value1: detail.value
       })
