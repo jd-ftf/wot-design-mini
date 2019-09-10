a75c900bce9713d7937eadccb7a469e5f4341f87
houxinchao <3104288823@qq.com>
2019-09-10 17:36:41 +0800

    noticeBar 动画效果完成

--- example/pages/noticeBar/noticeBar.js
+++ example/pages/noticeBar/noticeBar.js
@@ -1,7 +1,7 @@
 
 Page({
   data: {
-    value: 1,
+    text: "足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。",
     value1:1,
   },
   handleChange1 ({ detail }) {
--- packages/noticeBar/index.js
+++ packages/noticeBar/index.js
@@ -1,16 +1,109 @@
-Component({
-  properties: {
+const FONT_COLOR = '#ed6a0c';
+const BG_COLOR = '#fffbe8';
 
+Component({
+  
+  properties: {
+    ani:Object,
+    text:String,
+    textRect:Number,
+    wrapRect:Number,
+    delay: {
+      type: Number,
+      value: 1
+    },
+    speed: {
+      type: Number,
+      value: 100
+    },
+    scrollable: {
+      type: Boolean,
+      value: true
+    },
+    leftIcon: {
+      type: String,
+      value: ''
+    },
+    color: {
+      type: String,
+      value: FONT_COLOR
+    },
+    backgroundColor: {
+      type: String,
+      value: BG_COLOR
+    },
   },
   observers:{
- 
 
   },
-  created() {
-    
+  ready() {
+    this.init();
   },
 
   methods: {
- 
+    getRect(select) {
+      const _this = this;
+      return new Promise((resolve,reject)=>{
+        _this.createSelectorQuery()
+            .select(select)
+            .boundingClientRect(rects => {
+                if(rects){
+                    resolve(Math.ceil(rects.width));
+                }
+        }).exec();
+      })
+    },
+    init(){
+      Promise.all([
+        this.getRect('.jmd__noticeBarContainer--text'),
+        this.getRect('.jmd__noticeBarContainer--textWrapper')
+      ]).then( (arr) => {
+        this.setData ({
+          textRect : arr[0],
+          wrapRect : arr[1]
+        })        
+        this.scroll();
+      })
+    },
+    /**
+     * 滚动动画
+     * 如果外层宽度 大于 内层文字宽度 || 设定不滚动 ，则不需要动画
+     */
+    scroll(){
+      const { speed, scrollable, delay, wrapRect, textRect } = this.data;
+
+      if (scrollable && wrapRect < textRect) {
+        const duration = (textRect / speed) * 1000;
+        let scrollWidth = textRect + wrapRect;
+        this.initAnimation(duration, 'linear', delay, scrollWidth)
+      }
+    },
+    /**
+     * 动画初始化函数 
+     * @param duration 
+     * @param timingFunction 
+     * @param delay 
+     * @param scrollWidth 
+     */
+    initAnimation(duration, timingFunction, delay, scrollWidth) {
+      let animation = wx.createAnimation({
+        duration: duration,
+        timingFunction: timingFunction,
+        delay: delay
+      });
+      animation.translateX(-scrollWidth).step();
+      this.setData({
+        ani:  animation.export()
+      });
+    },
+    transitionEnd() {
+      const _this = this;
+      // 复位操作
+      this.initAnimation(0, 'linear', 0, 0);
+      // 延时，没有延时函数动画不生效
+      setTimeout(()=>{
+        _this.scroll();
+      },10);
+    },
   }
 });
