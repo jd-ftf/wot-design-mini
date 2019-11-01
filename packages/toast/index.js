15a51e5583c570c52b4d56a9b88d81191140a42a
gkxie <xieyu141419@jd.com>
2019-11-01 16:21:48 +0800

    loading: remove unuseful code

--- packages/toast/index.js
+++ packages/toast/index.js
@@ -1,5 +1,4 @@
 Component({
-  behaviors: [],
   properties: {
     iconName: String,
     iconClass: String,
@@ -9,39 +8,5 @@ Component({
       default: 'middle'
     },
     show: Boolean
-  },
-  data () {
-  },
-  // 在组件实例刚刚被创建时执行
-  created () {
-  },
-  // 在组件实例进入页面节点树时执行
-  attached () {
-  },
-  // 在组件在视图层布局完成后执行
-  ready () {
-  },
-  // 在组件实例被移动到节点树另一个位置时执行
-  moved () {
-  },
-  // 在组件实例被从页面节点树移除时执行
-  detached () {
-  },
-  // 每当组件方法抛出错误时执行
-  error () {
-  },
-
-  pageLifetimes: {
-    // 组件所在的页面被展示时执行
-    show () {
-    },
-    // 组件所在的页面被隐藏时执行
-    hide () {
-    },
-    // 组件所在的页面尺寸变化时执行
-    resize () {
-    }
-  },
-
-  methods: {}
+  }
 })
