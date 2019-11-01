20bdf8ba0a6c1ddce62491a700766d3ad3aed10d
gkxie <xieyu141419@jd.com>
2019-11-01 15:34:29 +0800

    toast: 用例编写完成

---
+++ example/pages/toast/index.js
@@ -0,0 +1,28 @@
+import Toast from '../../dist/toast/toast.js'
+
+Page({
+  showToast () {
+    Toast('提示信息')
+  },
+  showSuccessToast () {
+    Toast.success('操作成功')
+  },
+  showErrorToast () {
+    Toast.error('手机验证码输入错误，请重新输入')
+  },
+  showWarnToast () {
+    Toast.warning('提示信息')
+  },
+  showTopToast () {
+    Toast({
+      position: 'top',
+      msg: '提示信息'
+    })
+  },
+  showBottomToast () {
+    Toast({
+      position: 'bottom',
+      msg: '提示信息'
+    })
+  }
+})
