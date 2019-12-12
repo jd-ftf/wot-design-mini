6fa4ba8a702773d7641d0656a5e46921f25ae95a
gkxie <xieyu141419@jd.com>
2019-12-12 15:51:18 +0800

    feat(checkbox): complete

---
+++ packages/checkbox/index.js
@@ -0,0 +1,121 @@
+import VueComponent from '../common/component'
+
+VueComponent({
+  relations: {
+    '../checkboxGroup/index': {
+      type: 'parent',
+      linked (target) {
+        this.parent = target
+        this.checkName(this, this.data.value)
+      },
+      unlinked () {
+        this.parent = null
+      }
+    }
+  },
+  props: {
+    value: {
+      type: null,
+      observer (value) {
+        if (
+          value === null ||
+          value === undefined
+        ) {
+          throw Error('checkbox\'s value can\'t be null or undefined')
+        }
+        if (!this.data.inited) return
+        // 组合使用走这个逻辑
+        if (this.parent) {
+          this.checkName()
+          return this.parent.reset()
+        }
+        // 非组合使用走这个逻辑
+        if (typeof value !== 'boolean') {
+          throw Error('checkbox\'s value must be boolean without Group')
+        }
+        this.setData({ isChecked: value })
+      }
+    },
+    shape: {
+      type: String,
+      value: null,
+      observer (target) {
+        const type = ['circle', 'square', 'button']
+        if (type.indexOf(target) === -1) throw Error(`shape must be one of ${type.toString()}`)
+      }
+    },
+    checkedColor: {
+      type: String,
+      value: null
+    },
+    disabled: {
+      type: Boolean,
+      value: null
+    },
+    trueValue: String,
+    falseValue: String
+  },
+  data: {
+    isChecked: false,
+    inited: false
+  },
+  methods: {
+    /**
+     * @description 检测checkbox绑定的value是否和其它checkbox的value冲突
+     * @param {Object} self 自身
+     * @param  myName 自己的标识符
+     */
+    checkName (self = this, myName = this.data.value) {
+      this.parent && this.parent.children.forEach(node => {
+        if (
+          node !== self &&
+          node.data.value === myName
+        ) {
+          throw Error(`The checkbox's bound value: ${myName} has been used`)
+        }
+      })
+    },
+    /**
+     * @description 点击checkbox的Event handle
+     */
+    toggle (event) {
+      const { value, disabled, trueValue, falseValue, isChecked } = this.data
+      if (disabled) return
+      // 复选框单独使用时点击反选，并且在checkbox上触发change事件
+      if (!this.parent) {
+        this.setData({ value: !value })
+        return this.$emit('change',
+          !value ? (trueValue || true) : (falseValue || false)
+        )
+      }
+      /**
+       * 复选框组合使用时，如果之前已经被选中，那就把它从Group的value中剔除；
+       * 如果之前没有被选中，那就把它加入Group的value中；
+       * Group的value有observer，会自动重新匹配选中的checkbox
+       * 同时在Group上触发change事件
+       */
+      const temp = this.parent.data.value.slice(0)
+      if (isChecked) {
+        temp.splice(temp.indexOf(value), 1)
+      } else {
+        temp.push(value)
+      }
+      this.parent.setData({ value: temp })
+      this.parent.$emit('change', temp)
+    }
+  },
+  created () {
+    if (this.data.value === null) throw Error('checkbox\'s value must be set')
+    this.setData({ inited: true })
+  },
+  mounted () {
+    if (this.parent) {
+      // 组合使用走这个逻辑
+      return this.parent.reset()
+    } else if (typeof value !== 'boolean') {
+      // 非组合使用走这个逻辑
+      throw Error('checkbox\'s value must be boolean without Group')
+    }
+    this.setData({ isChecked: this.data.value })
+  }
+})
--- packages/common/component.md
+++ packages/common/component.md
@@ -4,10 +4,10 @@
 import VueComponent from '/packages/common/component.js'
 
 VueComponent({
-    props: {}, 
+    props: {},
     beforeCreate(){},
-    created(){}, 
-    mounted(){}, 
+    created(){},
+    mounted(){},
     destroyed(){}
 })
 ```
@@ -16,7 +16,7 @@ VueComponent({
 
 >如果使用封装的`VueComponent`组件，以下小程序`原生属性/方法`必须替换为对应的`封装属性/方法`
 
-| 封装          | 原生        | 
+| 封装          | 原生        |
 |--------------|----------- |
 | props        | properties |
 | beforeCreate | created    |
@@ -41,12 +41,13 @@ VueComponent({
 
 > 必须要弄清楚的声明周期，执行顺序按照降序排列。
 
-| hook         | 描述        | 
-|--------------|----------- |
-| properties   | 类型vue的props。 |
-| data         | 类型vue的data。 |
-| created      | 在组件实例刚刚被创建时执行，此时可以拿到this，但是不能调用setData。 |
-| attached     | 在组件实例进入页面节点树时执行，可以调用setData。 |
-| relations    | DOM build，父组件建立连接，双方可以在此hook互相操作对方。 |
-| ready        | DOM painted。 |
+| hook                    | 描述        |
+|-------------------------|----------- |
+| properties              | 类型vue的props。 |
+| data                    | 类型vue的data。 |
+| created                 | 在组件实例刚刚被创建时执行，此时可以拿到data，但是拿不到props，不能调用setData。 |
+| properties:observer     | 如果实例初始化时给properties传值，会触发observer，先于created执行 |
+| attached                | 在组件实例进入页面节点树时执行，可以调用setData。 |
+| relations               | DOM build，父组件建立连接，双方可以在此hook互相操作对方。 |
+| ready                   | DOM painted。 |
 
