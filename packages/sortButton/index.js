eacd720babacb94dda87192f3e936e234c8c238c
Gkxie <xieyu141419@jd.com>
2020-02-11 17:10:13 +0800

    update(sortButton): value——>title,direction——>value

--- packages/sortButton/index.js
+++ packages/sortButton/index.js
@@ -2,25 +2,25 @@ import VueComponent from '../common/component'
 
 VueComponent({
   props: {
-    value: String,
-    direction: {
+    title: String,
+    value: {
       type: String,
       value: '',
-      observe (direction) {
-        this.changeState(direction)
+      observe (value) {
+        this.changeState(value)
       }
     },
     only: {
       type: Boolean,
       observe () {
-        this.changeState(this.data.direction)
+        this.changeState(this.data.value)
       }
     },
     color: {
       type: String,
       value: '#0083ff',
       observe () {
-        this.changeState(this.data.direction)
+        this.changeState(this.data.value)
       }
     }
   },
@@ -35,11 +35,11 @@ VueComponent({
     }
   },
   methods: {
-    changeState (direction = '') {
+    changeState (value = '') {
       const { only, color } = this.data
       if (
-        this.data[direction] &&
-        this.data[direction].color === color
+        this.data[value] &&
+        this.data[value].color === color
       ) {
         return
       }
@@ -53,53 +53,53 @@ VueComponent({
           color: ''
         }
       }
-      if (only && !direction) {
+      if (only && !value) {
         data.top.display = false
         data.top.color = ''
         data.bottom.display = true
         data.bottom.color = ''
-      } else if (only && direction === 'top') {
+      } else if (only && value === 'top') {
         data.top.display = true
         data.top.color = color
         data.bottom.display = false
         data.bottom.color = ''
-      } else if (only && direction === 'bottom') {
+      } else if (only && value === 'bottom') {
         data.top.display = false
         data.top.color = ''
         data.bottom.display = true
         data.bottom.color = color
-      } else if (!only && direction === 'top') {
+      } else if (!only && value === 'top') {
         data.top.display = true
         data.top.color = color
         data.bottom.display = true
         data.bottom.color = ''
-      } else if (!only && direction === 'bottom') {
+      } else if (!only && value === 'bottom') {
         data.top.display = true
         data.top.color = ''
         data.bottom.display = true
         data.bottom.color = color
-      } else if (!only && !direction) {
+      } else if (!only && !value) {
         data.top.display = true
         data.top.color = ''
         data.bottom.display = true
         data.bottom.color = ''
       }
-      this.setData(Object.assign(data, { direction }))
+      this.setData(Object.assign(data, { value }))
     },
     handleClick () {
-      let { direction } = this.data
-      if (direction === 'top') {
-        direction = 'bottom'
-      } else if (direction === 'bottom') {
-        direction = 'top'
+      let { value } = this.data
+      if (value === 'top') {
+        value = 'bottom'
+      } else if (value === 'bottom') {
+        value = 'top'
       } else {
-        direction = 'top'
+        value = 'top'
       }
-      this.$emit('click', direction)
-      this.changeState(direction)
+      this.$emit('click', value)
+      this.changeState(value)
     }
   },
   created () {
-    this.changeState(this.data.direction)
+    this.changeState(this.data.value)
   }
 })
