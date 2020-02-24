55c6aed8577ff754e2de4209846c69bd0adac356
zhongzhijie <zhongzhijie@jd.com>
2020-02-24 18:38:35 +0800

    demo(inputNumber): 修正demo

--- example/pages/inputNumber/index.js
+++ example/pages/inputNumber/index.js
@@ -1,10 +1,52 @@
 Page({
   data: {
-    value: 2
+    value1: 1,
+    value2: 1,
+    value3: 1,
+    value4: 2,
+    value5: 1,
+    value6: '1.205',
+    value7: 1,
+    value8: 2
   },
-  handleChange ({ detail }) {
+  handleChange1 ({ detail }) {
     this.setData({
-      value: detail
+      value1: detail
+    })
+  },
+  handleChange2 ({ detail }) {
+    this.setData({
+      value2: detail
+    })
+  },
+  handleChange3 ({ detail }) {
+    this.setData({
+      value3: detail
+    })
+  },
+  handleChange4 ({ detail }) {
+    this.setData({
+      value4: detail
+    })
+  },
+  handleChange5 ({ detail }) {
+    this.setData({
+      value5: detail
+    })
+  },
+  handleChange6 ({ detail }) {
+    this.setData({
+      value6: detail
+    })
+  },
+  handleChange7 ({ detail }) {
+    this.setData({
+      value7: detail
+    })
+  },
+  handleChange8 ({ detail }) {
+    this.setData({
+      value8: detail
     })
   }
 })
