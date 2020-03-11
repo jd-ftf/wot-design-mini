31b0c9999fe563b6093699aa0b25750e66a1302b
zhongzhijie <zhongzhijie@jd.com>
2020-03-11 16:21:09 +0800

    fix(picker): 修复多级联动，修改内部值后取消恢复数据源；修复 column-change 异步情况下无法准确获取值的问题

--- example/pages/picker/index.js
+++ example/pages/picker/index.js
@@ -1,45 +1,15 @@
 import Toast from '../../dist/toast/toast.js'
 
 const district = {
-  0: [{
-    label: '北京',
-    value: '110000'
-  }, {
-    label: '广东省',
-    value: '440000'
-  }],
-  110000: [{
-    label: '北京',
-    value: '110100'
-  }],
-  440000: [{
-    label: '广州市',
-    value: '440100'
-  }, {
-    label: '深圳市',
-    value: '440300'
-  }],
-  110100: [{
-    label: '东城区',
-    value: '110101'
-  }, {
-    label: '西城区',
-    value: '110102'
-  }],
-  440100: [{
-    label: '荔湾区',
-    value: '440103'
-  }, {
-    label: '越秀区',
-    value: '440104'
-  }],
-  440300: [{
-    label: '罗湖区',
-    value: '440303'
-  }, {
-    label: '福田区',
-    value: '440304'
-  }]
+  '0': [{ label: '北京', value: '110000' }, { label: '广东省', value: '440000' }],
+  '110000': [{ label: '北京', value: '110100' }],
+  '440000': [{ label: '广州市', value: '440100' }, { label: '韶关市', value: '440200' }, { label: '深圳市', value: '440300' }, { label: '珠海市', value: '440400' }, { label: '汕头市', value: '440500' }],
+  '110100': [{ label: '东城区', value: '110101' }, { label: '西城区', value: '110102' }, { label: '朝阳区', value: '110105' }, { label: '丰台区', value: '110106' }, { label: '石景山区', value: '110107' }],
+  '440100': [{ label: '荔湾区', value: '440103' }, { label: '越秀区', value: '440104' }, { label: '海珠区', value: '440105'}],
+  '440200': [{ label: '武江区', value: '440203'}],
+  '440300': [{ label: '罗湖区', value: '440303' }, { label: '福田区', value: '440304' }],
+  '440400': [{ label: '香洲区', value: '440402' }, { label: '斗门区', value: '440403' }, { label: '金湾区', value: '440404' }],
+  '440500': [{ label: '龙湖区', value: '440507' }, { label: '金平区', value: '440511' }]
 }
 
 Page({
@@ -75,16 +45,15 @@ Page({
 
     columns7: ['选项1', '选项2', '选项3', '选项4', '选项5', '选项6', '选项7'],
 
-    onChangeDistrict (pickerView, value, columnIndex) {
+    onChangeDistrict (pickerView, value, columnIndex, resolve) {
       const item = value[columnIndex]
       if (columnIndex === 0) {
         pickerView.setColumnData(1, district[item.value])
         pickerView.setColumnData(2, district[district[item.value][0].value])
-        return
-      }
-      if (columnIndex === 1) {
+      } else if (columnIndex === 1) {
         pickerView.setColumnData(2, district[item.value])
       }
+      resolve()
     },
 
     displayFormat (items) {
