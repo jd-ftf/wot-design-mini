d408af3b752c82d698c5267b778db8b432310473
xieyu <gkxie@qq.com>
2020-04-21 14:49:41 +0800

    config: 组件库配置更新 (#104)

--- build/iconInit.js
+++ build/iconInit.js
@@ -1,10 +1,10 @@
-var postcss = require('postcss');
-var fs = require('fs');
-var path = require('path');
-var render = require('json-templater/string');
-var fontFile = fs.readFileSync(path.resolve(__dirname, '../packages/icon/index.scss'), 'utf8');
-var nodes = postcss.parse(fontFile).nodes;
-var classList = [];
+var postcss = require('postcss')
+var fs = require('fs')
+var path = require('path')
+var render = require('json-templater/string')
+var fontFile = fs.readFileSync(path.resolve(__dirname, '../packages/icon/index.scss'), 'utf8')
+var nodes = postcss.parse(fontFile).nodes
+var classList = []
 
 var template = `
 Page({
@@ -15,17 +15,17 @@ Page({
 `
 
 nodes.forEach((node) => {
-  var selector = node.selector || '';
-  var reg = new RegExp(/\.wd-icon-([^:]+):before/);
-  var arr = selector.match(reg);
+  var selector = node.selector || ''
+  var reg = new RegExp(/\.wd-icon-([^:]+):before/)
+  var arr = selector.match(reg)
 
   if (arr && arr[1]) {
-    classList.push(arr[1]);
+    classList.push(arr[1])
   }
-});
+})
 
 template = render(template, {
   list: JSON.stringify(classList)
 })
 
-fs.writeFile(path.resolve(__dirname, '../example/pages/icon/index.js'), template, () => {});
+fs.writeFile(path.resolve(__dirname, '../example/pages/icon/index.js'), template, () => { })
