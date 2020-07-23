e72fdd74030d5f928e2a86fa95e3bc8cb4ad4051
xieyu141419 <xieyu141419@jd.com>
2020-07-23 18:07:40 +0800

    fix(resize): 固高固宽父元素限制子元素尺寸变化

--- packages/resize/index.js
+++ packages/resize/index.js
@@ -5,7 +5,10 @@ VueComponent({
     expandScrollTop: 0,
     shrinkScrollTop: 0,
     expandScrollLeft: 0,
-    shrinkScrollLeft: 0
+    shrinkScrollLeft: 0,
+    position: 'static',
+    height: 0,
+    width: 0
   },
   methods: {
     onScrollHandler () {
@@ -14,21 +17,39 @@ VueComponent({
       // 计算 scrollTop 的极限值
       const maxHeight = 10000 * (height)
       const maxWidth = 10000 * (width)
-      // 所有元素滚动到底部
-      this.setData({
+      const target = {
         expandScrollTop: maxHeight,
         shrinkScrollTop: maxHeight,
         expandScrollLeft: maxWidth,
         shrinkScrollLeft: maxWidth
-      })
+      }
+      // 性能优化
+      const diff = Object.keys(target).reduce((prev, key) => {
+        if (target[key] !== this.data[key]) {
+          prev[key] = target[key]
+        }
+        return prev
+      }, {})
+      if (Object.keys(diff).length === 0) return
+      // 所有元素滚动到底部
+      this.setData(diff)
     }
   },
   mounted () {
     const query = this.createSelectorQuery()
       .in(this)
-      .select('.wd-resize')
+      .select('.wd-resize__container')
       .boundingClientRect()
     query.exec(([res]) => {
+      // 插槽脱离父容器文档流，需要手动设置父容器高宽，防止父容器坍塌。
+      this.setData({
+        // 初次挂载时先使用文档流，
+        // 在此 event loop 中 可以获得正常的 layout 计算结算，
+        // 在下一个帧(n 个 event loop)之后修改为绝对定位。脱离文档流，杜绝被父元素的样式影响。
+        position: 'absolute',
+        height: res.height,
+        width: res.width
+      })
       // 闭包记录容器高度
       let lastHeight = res.height
       let lastWidth = res.width
@@ -38,6 +59,11 @@ VueComponent({
       })
       this.onScrollHandler = () => {
         query.exec(([res]) => {
+          // 手动设置父容器高宽，防止父容器坍塌
+          this.setData({
+            height: res.height,
+            width: res.width
+          })
           // 滚动完，重新获取容器新的高度
           const newHeight = res.height || 1
           const newWidth = res.width || 1
