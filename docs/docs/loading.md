4374a39684b6c2f594327e776e977bb60972a73c
xieyu141419 <xieyu141419@jd.com>
2020-07-31 10:43:37 +0800

    perf(loading): 品牌升级

--- docs/docs/loading.md
+++ docs/docs/loading.md
@@ -12,34 +12,56 @@
 
 ### 基本用法
 
-默认为 `circle` 类型的加载指示器。
+默认为 `circle-outline` 类型的加载指示器。
 
 ```html
 <wd-loading />
 ```
 
-### 修改类型
+### 修改指示器类型
 
-通过 `type` 修改指示器的类型，可选值为 'spinner' 和 'circle'，默认为 'circle'。
+通过 `type` 修改指示器的类型，可选值为 'spinner' / 'circle' / 'circle-outline'，默认为 'circle-outline'。
 
 ```html
 <wd-loading type="spinner" />
+<wd-loading type="circle" />
+```
+
+### 修改颜色
+
+通过 `color` 属性修改指示器的颜色。比如修改为白色，同时设置背景为黑色。
+
+```html
+<wd-loading color="{{#fff}}" custom-class="loading-black" type="circle"/>
+```
+```css
+.loading-black {
+  background: rgba(0, 0, 0, 0.7);
+  padding: 10px;
+  border-radius: 4px;
+}
 ```
 
 ### 修改指示器大小
 
-通过 `size` 属性设置指示器的大小，默认为 '32px' 大小。
+通过 `size` 属性设置指示器的大小，默认为大小 '32'，单位固定为 `px`。
 
 ```html
-<wd-loading size="20px" />
-<wd-loading size="30px" />
-<wd-loading size="50px" />
+<wd-loading size="20" />
+<wd-loading size="30" />
+<wd-loading size="50" />
 ```
 
 ### Attributes
 
 | 参数      | 说明                                 | 类型      | 可选值       | 默认值   |
 |---------- |------------------------------------ |---------- |------------- |-------- |
-| type      |	加载指示器类型                        |	string    |	'spinner'   |	'circle'  |
-| color	    | 设置加载指示器颜色                      |	string    |	-         |	'#c5c5c5' |
-| size      | 设置加载指示器大小                      | string    | -          | '32px'   |
+| type | 加载指示器类型 | string | 'spinner'/'circle-outline'/'circle' | 'circle-outline' |
+| color | 设置加载指示器颜色 | string | - | '#4D80F0' |
+| size | 设置加载指示器大小 | number | - | 32 |
+
+### 外部样式类
+
+| 类名     | 说明                |
+|---------|---------------------|
+| custom-class | 根结点样式 |
--- packages/loading/index.js
+++ packages/loading/index.js
@@ -1,16 +1,44 @@
 import VueComponent from '../common/component'
+import base64 from '../common/base64'
+
+const icon = {
+  'circle-outline' (color = '#4D80F0') {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><defs><linearGradient x1="100%" y1="0%" x2="0%" y2="0%" id="a"><stop stop-color="#FFF" offset="0%" stop-opacity="0"/><stop stop-color="#FFF" offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><path d="M21 1c11.046 0 20 8.954 20 20s-8.954 20-20 20S1 32.046 1 21 9.954 1 21 1zm0 7C13.82 8 8 13.82 8 21s5.82 13 13 13 13-5.82 13-13S28.18 8 21 8z" fill="${color}"/><path d="M4.599 21c0 9.044 7.332 16.376 16.376 16.376 9.045 0 16.376-7.332 16.376-16.376" stroke="url(#a)" stroke-width="3.5" stroke-linecap="round"/></g></svg>`
+  },
+  circle (color = '#000000') {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-23 -23 46 46"><circle cx="0" cy="0" r="20" stroke="${color}" stroke-width="3" fill="none"><animate attributeName="stroke-dasharray" begin="0s" dur="1.5s" repeatCount="indefinite" values="1 200;90 150;90 150" keyTimes="0;0.5;1"/><animate attributeName="stroke-dashoffset" begin="0s" dur="1.5s" repeatCount="indefinite" values="0;-40;-120" keyTimes="0;0.5;1"/></circle></svg>`
+  }
+}
 
 VueComponent({
+  data: {
+    svg: ''
+  },
   props: {
-    size: {
-      type: String,
-      value: '32px'
-    },
     type: {
       type: String,
-      value: 'circle'
+      value: 'circle-outline'
     },
-    color: String,
-    customStyle: String
+    color: {
+      type: String,
+      value: '#4D80F0',
+      observer: 'buildSvg'
+    },
+    size: {
+      type: Number,
+      value: 32
+    }
+  },
+  methods: {
+    buildSvg () {
+      const { type, color } = this.data
+      if (type !== 'circle-outline' && type !== 'circle') return
+      const svg = icon[type](color)
+      const svgStr = `"data:image/svg+xml;base64,${base64(svg)}"`
+      this.setData({ svg: svgStr })
+    }
+  },
+  created () {
+    this.buildSvg()
   }
 })
