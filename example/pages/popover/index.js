11c8b920249e0821d24cf8e66f22b8cd61597c9a
houxinchao@jd.com <houxinchao@jd.com>
2020-08-12 16:21:30 +0800

    feat(tooltip): 文字提示组件升级
    demo: 文字提示demo修改
    
    feat(popover): 新增popober组件
    
    fix: format demo-block
    
    docs: popover文档添加
    
    docs: 文档修改
    
    fix: 缩进问题

---
+++ example/pages/popover/index.js
@@ -0,0 +1,28 @@
+import Toast from '../../dist/toast/toast'
+
+Page({
+  data: {
+    menu: [
+      {
+        iconClass: 'read',
+        content: '全部标记已读'
+      },
+      {
+        iconClass: 'delete',
+        content: '清空最近会话'
+      },
+      {
+        iconClass: 'detection',
+        content: '消息订阅设置'
+      },
+      {
+        iconClass: 'subscribe',
+        content: '消息异常检测'
+      }
+    ]
+  },
+  link (param) {
+    const data = param.detail.item
+    Toast('选择了' + data.content)
+  }
+})
